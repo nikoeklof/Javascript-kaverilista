1f96438484dea6afa820734e907d88b8ce9b37a2
Niko Eklöf <80674961+nikoeklof@users.noreply.github.com>
2021-04-28 10:10:38 +0300

    Update kaverilista.js
    poistettu turhia console.log syntaxeja

--- kaverilista.js
+++ kaverilista.js
@@ -6,7 +6,6 @@ lomake.addEventListener('submit', uusiListaElementti)
 
 function uusiListaElementti(event) {
     event.preventDefault()
-    console.log("painoi nappia")
     let elementinnimi = document.querySelector('.container input[type="text"]').value
     if (elementinnimi < 1) {
         alert('Anna nimi!')
@@ -21,12 +20,8 @@ function uusiListaElementti(event) {
 }
 
 function poistaItem() {
-    console.log("painoi poista")
     let elementinnimi = document.querySelector('.container input[type="text"]').value
-    console.log(elementinnimi)
-
     let lista = document.querySelectorAll('.list-item')
-    console.log(lista[0].textContent)
     if (lista.length > 0) {
         for (var i = 0; i < lista.length; i++) {
             console.log(i)
